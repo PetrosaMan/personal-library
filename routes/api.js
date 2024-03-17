6d6ee7ac241954b1192ee41f0fee4c82ff697e6f
PetrosaMan <dw.todd.eng@gmail.com>
2024-03-17 22:39:55 +0200

    Initial code attempt for GET a book from id

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,6 @@ const mongodb = require("mongodb");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 // Connect mongoose to mongoDB
 mongoose
   .connect(process.env.DB)
@@ -24,10 +23,10 @@ mongoose
   });
 
 // mongoose bookSchema
-const bookSchema = new Schema({    
-  title: { type: String, required: true },  
+const bookSchema = new Schema({
+  title: { type: String, required: true },
   comments: [String],
-  commentcount: Number
+  commentcount: Number,
 });
 
 // define mongoose model
@@ -38,41 +37,36 @@ module.exports = { Book };
 module.exports = function (app) {
   app
     .route("/api/books")
-    .get( async function (req, res) { 
-      let arrayOfBooks;      
-      await Book.find({})
-      .then((arrayOfBooks) => {        
-          return res.json(arrayOfBooks);
-      });       
-    })     
-
-    .post( async function (req, res) {
+    .get(async function (req, res) {
+      let arrayOfBooks;
+      await Book.find({}).then((arrayOfBooks) => {
+        return res.json(arrayOfBooks);
+      });
+    })
+    .post(async function (req, res) {
       let title = req.body.title;
-      
-      if(!title) {
-        return res.status(400).json({ error: "missing required field title"});
-      };
-
-      if (await Book.exists({title: title})) {
-        return res.status(400).json({ error: title + " already exists in library" });
+      if (!title) {
+        return res.status(400).json({ error: "missing required field title" });
       }
-      
+      if (await Book.exists({ title: title })) {
+        return res
+          .status(400)
+          .json({ error: title + " already exists in library" });
+      }
       try {
         // create new book
-        let newBook = new Book ({
-          title: title,          
-          comments:[],
-          commentcount: 0
+        let newBook = new Book({
+          title: title,
+          comments: [],
+          commentcount: 0,
         });
-        
-        // save new book
         await newBook.save();
-        // respond with saved book        
-        return res.status(200).json({ title:newBook.title, _id: newBook._id});
-     } catch {
+        // respond with saved book
+        return res.status(200).json({ title: newBook.title, _id: newBook._id });
+      } catch {
         // Handle errors
-        res.status(500).json({ error: "Internal server error"});
-     }       
+        res.status(500).json({ error: "Internal server error" });
+      }
     })
 
     .delete(function (req, res) {
@@ -81,36 +75,41 @@ module.exports = function (app) {
 
   app
     .route("/api/books/:id")
-    .get(function (req, res) {
+    .get( async function (req, res) {
       let bookid = req.params.id;
+      try {
+        const foundBook = await Book.findById(bookid);
+        if(!foundBook) {
+          return res.json("No book exists");
+        }
+        return res.json({ foundBook });
+      } catch (error) {
+          res.json("error");
+      }
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
 
-    .post( async function (req, res) {
+    .post(async function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
-      console.log(comment)
-      console.log(bookid);
-      
-      try {
-      const foundBook = await Book.findById(bookid);
-
-      if(!foundBook) {
-        console.log('Book not found');
-        return;
-      }
-
-      foundBook.comments.push(comment); // add comment
-      foundBook.commentcount += 1;
-      // save updated book
-      const updatedBook = await foundBook.save();
-      console.log("Updated book", updatedBook)
-    } catch (error) { 
-        console.error("Error updating book: ", error);
-    }      
-      //if(!comment) {
-      //  return res.status(400).json("missing required field comment");
-      //}           
+      //console.log('Comment: ',comment);
+      //console.log(bookid);       
+      try {  
+        if(!comment) {
+          return res.json("missing required field comment")
+        }  
+        const foundBook = await Book.findById(bookid);
+        if (!foundBook) {
+          return res.json("no book exists");          
+        }
+        foundBook.comments.push(comment); // add comment
+        foundBook.commentcount += 1;      // increment count
+        // save updated book
+        const updatedBook = await foundBook.save();
+        return res.json({ updatedBook });
+      } catch (error) {
+         res.json("error updating book");
+      }      
       //json res format same as .get
     })
 
